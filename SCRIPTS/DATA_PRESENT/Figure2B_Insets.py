801aeca74b782a87a832cf1aa2f25624a2846fb7
Will <werobinson6@gmail.com>
2021-06-22 15:33:26 +0200

    runs in the folder

--- SCRIPTS/DATA_PRESENT/Figure2B_Insets.py
+++ SCRIPTS/DATA_PRESENT/Figure2B_Insets.py
@@ -1,44 +1,60 @@
+import sys
 import numpy as np
+import pandas as pd
 from pathlib import Path
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+
+# add the SCRIPTS directory to the system path
+# so that its contents can be imported
+script_dir = Path(__file__).parents[1].as_posix()
+sys.path.append(script_dir)
+# get the repository directory for file output
+repository_dir = Path(__file__).parents[2]
 
 from NorthNet import info_params
 from NorthNet.calculations import calculations
 from NorthNet.network_manipulations.networkx_ops import coordinates as c_ops
 
-import __init__
-from helpers import load_series
 from helpers.network_load_helper import load_from_edge_list
 from helpers.network_load_helper import load_coordinates_list
-from helpers.loading_helper import exp_info, modified_averages, prime_header
+from helpers.loading_helper import get_carbon_inputs
+
+data_folder = repository_dir/'DATA'
+derived_parameters_dir = data_folder/'DERIVED_PARAMETERS'
+plot_folder = repository_dir/'PLOTS'
+report_directory = data_folder/'DATA_REPORTS'
+exp_info_dir = repository_dir/"EXPERIMENT_INFO/Experiment_parameters.csv"
+
+exp_info = pd.read_csv(exp_info_dir, index_col = 0)
 
 select_conditions = ['[C=O]/ M', '[CaCl2]/ M', '[NaOH]/ M','[O=C(CO)CO]/ M',
                     'residence_time/ s', 'temperature/ oC']
 condition_names = ['[formaldehyde]/ mM', '[CaCl$_2$]/ mM',
                    '[NaOH]/ mM','[dihydroxyacetone]/ mM', 'Residence time/ s',
                    'Temperature/ $^\circ$C', '[CaCl$_2$]/[NaOH]']
+
 condition_dict = {}
 for s in select_conditions:
     condition_dict[s] = {}
-    for e in exp_info:
-        condition_dict[s][e] = exp_info[e].parameters[s]
+    for e in exp_info.index:
+        condition_dict[s][e] = exp_info.loc[e,s]
 
 condition_dict['Ca_OH_ratio'] = {}
-for e in exp_info:
-    condition_dict['Ca_OH_ratio'][e] = exp_info[e].parameters['[CaCl2]/ M']/exp_info[e].parameters['[NaOH]/ M']/1000
+for e in exp_info.index:
+    condition_dict['Ca_OH_ratio'][e] = exp_info.loc[e,'[CaCl2]/ M']/exp_info.loc[e,'[NaOH]/ M']/1000
 
-edge_list = 'information_sources/dendrogram_edgelist.csv'
-coord_list = 'information_sources/dendrogram_coordinates.csv'
+edge_list = repository_dir/'RESOURCES/dendrogram_edgelist.csv'
+coord_list = repository_dir/'RESOURCES/dendrogram_coordinates.csv'
 G = load_from_edge_list(edge_list)
 pos = load_coordinates_list(coord_list)
 c_ops.set_network_coords(G,pos)
 c_ops.normalise_network_coordinates(G)
-c_ops.rotate_network(G, -np.pi/4 - np.pi/6)
+# c_ops.rotate_network(G, -np.pi/4 - np.pi/6)
 lines = c_ops.get_network_lineplot(G)
 dots  = c_ops.get_network_scatter(G)
 
-factor = 1000
+factor = 800
 for c,v in enumerate(condition_dict,0):
     fig, ax = plt.subplots(figsize = (3.13/2.54,2.81/2.54), frameon = False)
     ax.plot(lines[0],lines[1],
@@ -48,7 +64,7 @@ for c,v in enumerate(condition_dict,0):
     leaf_x = []
     leaf_y = []
     for n in G.nodes:
-        if n in exp_info:
+        if n in exp_info.index:
             xy = G.nodes[n]['pos']
             leaf_x.append(xy[0])
             leaf_y.append(xy[1])
@@ -72,11 +88,5 @@ for c,v in enumerate(condition_dict,0):
     ax.set_axis_off()
     ylm = ax.get_ylim()
     ax.set_ylim(ylm[1],ylm[0])
-    plt.savefig('plots_for_paper/{}_dendrogam.png'.format(v.split('/')[0]), dpi = 600)
+    plt.savefig(repository_dir/'PLOTS/{}_dendrogam.png'.format(v.split('/')[0]), dpi = 600)
     plt.close()
-
-
-# ax[0].set_position([0.1, 0.375, 0.85, 0.575])
-# for x in range(1,len(ax)):
-#     ax[x].set_position([0.025 + ((x-1)*(0.95/len(select_conditions))), 0.1,
-#                     1/(len(select_conditions)+1), 0.25])
