b3872ab1d87d8cdaf06bb66734b84659dcce3d38
Will <werobinson6@gmail.com>
2021-06-22 14:30:40 +0200

    script works in repository structure

--- SCRIPTS/DATA_PRESENT/bar_chart_panel.py
+++ SCRIPTS/DATA_PRESENT/bar_chart_panel.py
@@ -1,35 +1,51 @@
+import sys
+from pathlib import Path
+
+# add the SCRIPTS directory to the system path
+# so that its contents can be imported
+script_dir = Path(__file__).parents[1].as_posix()
+sys.path.append(script_dir)
+# get the repository directory for file output
+repository_dir = Path(__file__).parents[2]
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-import __init__
-from helpers.loading_helper import data, errors
-from helpers.loading_helper import clrs
-from helpers.loading_helper import header
-from helpers.loading_helper import exp_info
+import helpers.chem_info as info_params
+from helpers.loading_helper import get_carbon_inputs
 
-exp_list = [*exp_info]
+data_folder = repository_dir/'DATA'
+derived_parameters_dir = data_folder/'DERIVED_PARAMETERS'
+plot_folder = repository_dir/'PLOTS'
+
 data_set_selections = ['FRN094C', 'FRN089A', 'FRN090B','FRN077C']
-indices = [exp_list.index(s) for s in data_set_selections]
 
-plot_stack = np.zeros((len(data_set_selections), len(data[0])))
-error_stack = np.zeros((len(data_set_selections), len(data[0])))
-for x,i in enumerate(indices):
-    plot_stack[x] = data[i]
-    error_stack[x] = errors[i]
+df = pd.read_csv(derived_parameters_dir/'AverageData.csv', index_col = 0)
+# remove empty columns
+df = df.dropna(axis = 1)
 
-idx = np.argwhere(np.all(plot_stack[..., :] == 0, axis=0))
-plot_stack = np.delete(plot_stack, idx, axis = 1)
-error_stack = np.delete(error_stack, idx, axis = 1)
+# create a data frame with the selected compounds
+sel = df.loc[data_set_selections]
+# remove columns containing only zeros
+sel = sel.loc[:, (sel != 0).any(axis=0)]
 
-comp_ax = [header[i] for i,c in enumerate(header) if i not in idx]
-comp_clrs = [clrs[i] for i,c in enumerate(clrs) if i not in idx]
-comp_numbers = [i for i,c in enumerate(header) if i not in idx]
+# convert to numpy
+plot_stack = sel.to_numpy()
 
-class margins: left = 0.2;right = 0.05;top = 0.15;bottom = 0.13;
+comp_ax = [x.split('/')[0] for x in sel.columns]
+comp_clrs  = [info_params.colour_assignments[x] for x in comp_ax]
+comp_clrs = [info_params.colour_assignments[x] for x in comp_ax]
+comp_numbers = [info_params.compound_numbering[x] for x in comp_ax]
+
+# define plot parameters
+class margins: left = 0.15;right = 0.03;top = 0.15;bottom = 0.025;
 left = margins.left
 width = 1-margins.left-margins.right
 height = (1-margins.top-margins.bottom)/len(data_set_selections)
 axis_label_font_size = 6
+
+# create the plot
 fig, ax = plt.subplots(nrows = len(data_set_selections),
                         figsize = (7.17/2.54,3.75/2.54))
 
@@ -46,15 +62,17 @@ for x in range(0,len(plot_stack)):
     ax[x].set_position([left, bottom, width, height])
 
 for a in ax:
-    a.tick_params(axis = 'both', which = 'both', length = 2,
-                        labelsize = 6)
-
+    a.tick_params(axis = 'both', which = 'both',
+                length = 2,
+                labelsize = 4,
+                pad = 1)
+    a.set_xticklabels([])
 
 fig.text((margins.left - margins.right + 1)/2, 0.04,
-         'Compound number', ha='center',
+         'Compound', ha='center',
          fontsize = axis_label_font_size)
-fig.text(0.04, 0.53, 'log$_{10}$(concentration/ M)',
-            va='center', rotation='vertical',
+fig.text(margins.left/3, 0.57, 'log$_{10}$(concentration/ M)',
+            va='center', ha = 'center', rotation='vertical',
             fontsize = axis_label_font_size)
 
-plt.savefig('plots_for_paper/Bar_charts.png', dpi = 600)
+plt.savefig(plot_folder/'Bar_charts.png', dpi = 600)
