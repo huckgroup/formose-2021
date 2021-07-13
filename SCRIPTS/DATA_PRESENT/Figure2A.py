1a94ddcbd273e47630bc38318a39be464793e50f
Will <werobinson6@gmail.com>
2021-07-13 11:33:07 +0200

    increased figure size

--- SCRIPTS/DATA_PRESENT/Figure2A.py
+++ SCRIPTS/DATA_PRESENT/Figure2A.py
@@ -37,15 +37,15 @@ comp_clrs  = [info_params.colour_assignments[x] for x in comp_ax]
 comp_numbers = [info_params.compound_numbering[x] for x in comp_ax]
 
 # define plot parameters
-class margins: left = 0.15;right = 0.03;top = 0.15;bottom = 0.025;
+class margins: left = 0.2;right = 0.03;top = 0.15;bottom = 0.025;
 left = margins.left
 width = 1-margins.left-margins.right
 height = (1-margins.top-margins.bottom)/len(data_set_selections)
-axis_label_font_size = 6
+axis_label_font_size = 9
 
 # create the plot
 fig, ax = plt.subplots(nrows = len(data_set_selections),
-                        figsize = (7.17/2.54,3.75/2.54))
+                        figsize = (7.4/2.54,4.0/2.54))
 
 for x in range(0,len(plot_stack)):
     ax[x].bar(comp_numbers, plot_stack[x],
@@ -59,17 +59,30 @@ for x in range(0,len(plot_stack)):
     bottom = margins.bottom + (x+1)*(1-margins.top-margins.bottom)/len(data_set_selections) - height/2
     ax[x].set_position([left, bottom, width, height])
 
+
 for a in ax:
-    a.tick_params(axis = 'both', which = 'both',
+    a.tick_params(axis = 'y', which = 'major',
                 length = 2,
-                labelsize = 4,
-                pad = 1)
+                labelsize = 7,
+                pad = 1,
+                )
+    a.tick_params(axis = 'both', which = 'minor',
+                length = 1,
+                labelsize = 7,
+                pad = 1,
+                )
+    a.tick_params(axis = 'x', which = 'major',
+                length = 1,
+                labelsize = 7,
+                pad = 1,
+                )
+
     a.set_xticklabels([])
 
 fig.text((margins.left - margins.right + 1)/2, 0.04,
          'Compound', ha='center',
          fontsize = axis_label_font_size)
-fig.text(margins.left/3, 0.57, 'log$_{10}$(concentration/ M)',
+fig.text(margins.left/3, 0.5, 'log$_{10}$(concentration/ M)',
             va='center', ha = 'center', rotation='vertical',
             fontsize = axis_label_font_size)
 
