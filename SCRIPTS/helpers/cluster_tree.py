0050f3152cc25dd187d24fa4936d3858972ff719
Will <werobinson6@gmail.com>
2021-07-27 16:09:38 +0200

    function for creating hierarchical json

--- SCRIPTS/helpers/cluster_tree.py
+++ SCRIPTS/helpers/cluster_tree.py
@@ -25,3 +25,49 @@ def graph_from_linkage(linkage_mat, id_modifier = ''):
                         dist = abs(n.right.dist - n.dist))
 
     return G
+
+def createNestedJSON(linkage_mat):
+    '''
+    Create a JSON string resembling the following:
+    {
+      name: "root",
+      children: [
+        {name: "child #1"},
+        {
+          name: "child #2",
+          children: [
+            {name: "grandchild #1"},
+            {name: "grandchild #2"},
+            {name: "grandchild #3"}
+          ]
+        }
+      ]
+    }
+    linkage_mat: scipy linkage matrix
+
+    json_string: str
+        JSON string
+    '''
+    import json
+
+    def connectNode(node, parent):
+        new_entry = {"name":node.id, 'children':[]}
+        parent["children"].append(new_entry)
+
+        if node.left:
+            connectNode(node.left, new_entry)
+        if node.right:
+            connectNode(node.right, new_entry)
+
+    rootnode, nodelist = hierarchy.to_tree(linkage_mat, rd= True)
+
+    root_name = rootnode.id
+
+    json_build = {"name":root_name, 'children':[]}
+
+    connectNode(rootnode.left, json_build)
+    connectNode(rootnode.right, json_build)
+
+    json_string = json.dumps(json_build)
+
+    return json_string
