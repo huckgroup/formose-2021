fe79a91af7a8cefe5816280987f1aa783d384ab1
Will <werobinson6@gmail.com>
2021-07-27 16:38:07 +0200

    file not used.

--- SCRIPTS/helpers/load_series.py
+++
@@ -1,9 +0,0 @@
-def load_series_sequences(fname):
-
-    series_dict = {}
-    with open(fname, 'r') as f:
-        for line in f:
-            ins = line.strip('\n').split(',')
-            series_dict[ins[0]] = [x for x in ins[1:] if x != '']
-
-    return series_dict
