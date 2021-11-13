34b7fbea03415724b3e05514e38c6c2d4ac6bc15
Will <werobinson6@gmail.com>
2021-11-13 20:03:05 +0100

    added annotations, remove unused import

--- SCRIPTS/helpers/parameter_determination_helper.py
+++ SCRIPTS/helpers/parameter_determination_helper.py
@@ -1,22 +1,28 @@
-'''
-Function for determining concentration amplitudes from data.
-'''
 import numpy as np
 from scipy import fftpack
 from scipy import interpolate
-from scipy.optimize import curve_fit
 
 def fourier_transform(X,Y):
+    '''
+    Function for determining concentration amplitudes from data.
+    '''
     # interpolate axes to get even time steps
     f = interpolate.interp1d(X, Y, kind = "linear")
 
+    # get timestep from average of timepoints
     T = np.diff(X).mean()
+    # create new time axis with time step
     new_x = np.arange(X.min(), X.max(), T)
+    # cut the new x axis down so it does not
+    # exceed the range of the experimental 
+    # x axis.
     idx = np.where(new_x < X.max())
     new_x = new_x[idx]
 
+    # create interpolated signal
     new_y = f(new_x)
 
+    # Perform fourier transform
     N = len(new_x)
 
     yf = fftpack.fft(new_y)
