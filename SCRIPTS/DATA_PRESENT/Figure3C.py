807ea20b8ad8e34ef964daad4fc86262da0c0c65
Will <werobinson6@gmail.com>
2021-11-14 23:56:57 +0100

    working on figure 3c

--- SCRIPTS/DATA_PRESENT/Figure3C.py
+++ SCRIPTS/DATA_PRESENT/Figure3C.py
@@ -44,7 +44,7 @@ amplitude_data = amplitude_data.dropna(axis = 1)
 # loading in the formose reaction as a NorthNet Network Object
 formose_file = repository_dir/'FORMOSE_REACTION/FormoseReactionNetwork.pickle'
 with open(formose_file, 'rb') as f:
-	FormoseNetwork = pickle.load(f)
+    FormoseNetwork = pickle.load(f)
 
 series_sel = 'Formaldehyde_2_series'
 file_name = 'Figure3C'
@@ -53,7 +53,6 @@ file_name = 'Figure3C'
 data_keys = series_seqs.loc[series_sel]
 data_set_selections = list(data_keys.dropna())
 data_set_selections = [data_set_selections[1], data_set_selections[-1]]
-print(data_set_selections)
 
 # load in the reaction lists determined for
 # modulated data sets.
@@ -61,31 +60,31 @@ print(data_set_selections)
 # add network reactions into a
 networks = {}
 for e in exp_info.index:
-	for d in data_set_selections:
-		fname = '{}_reaction_list.txt'.format(d)
-		with open(reaction_list_directory/fname, 'r') as f:
-			for line in f:
-				lines = f.readlines()
-		rxns = []
-		for l in lines:
-			rxns.append(FormoseNetwork.NetworkReactions[l.strip('\n')])
+    for d in data_set_selections:
+        fname = '{}_reaction_list.txt'.format(d)
+        with open(reaction_list_directory/fname, 'r') as f:
+            for line in f:
+                lines = f.readlines()
+        rxns = []
+        for l in lines:
+            rxns.append(FormoseNetwork.NetworkReactions[l.strip('\n')])
 
-		n_net = Classes.Network(rxns, e, '')
+        n_net = Classes.Network(rxns, e, '')
 
-		networks[d] = convert_to_networkx(n_net)
+        networks[d] = convert_to_networkx(n_net)
 
 # remove secondary reactant nodes
 node_removals = ['C=O', 'O', '[OH-]']
 for n in networks:
-	[networks[n].remove_node(node) for node in node_removals
-										if node in networks[n].nodes]
+    [networks[n].remove_node(node) for node in node_removals
+                                        if node in networks[n].nodes]
 # create a network merging all of the networks
 F = nx.DiGraph()
 for n in networks:
-	F = nx.compose(F,networks[n])
+    F = nx.compose(F,networks[n])
 
 # create a layout for F (this will be the layout for each plotted network)
-pos = graphviz_layout(F, render_engine = 'neato')
+pos = graphviz_layout(F, render_engine = 'sfdp')
 
 # use F to process coordinate system
 c_ops.set_network_coords(F,pos)
@@ -98,36 +97,36 @@ base_net_plot = c_ops.get_network_lineplot(F)
 pos_norm = {n:F.nodes[n]['pos'] for n in F}
 
 for n in networks:
-	c_ops.set_network_coords(networks[n],pos_norm)
+    c_ops.set_network_coords(networks[n],pos_norm)
 
 '''Add colouring information into the networks'''
 for n in networks:
-	for node in networks[n].nodes:
-		if '>>' in node:
-			networks[n].nodes[node]['color'] = "#000000"
-		else:
-			networks[n].nodes[node]['color'] = info_params.colour_assignments[node]
+    for node in networks[n].nodes:
+        if '>>' in node:
+            networks[n].nodes[node]['color'] = "#000000"
+        else:
+            networks[n].nodes[node]['color'] = info_params.colour_assignments[node]
 
 for n in networks:
-	for edge in networks[n].edges:
-		for e in edge:
-			if '>>' in e:
-				col = info_params.reaction_colours[e]
-				networks[n].edges[edge]['color'] = col
+    for edge in networks[n].edges:
+        for e in edge:
+            if '>>' in e:
+                col = info_params.reaction_colours[e]
+                networks[n].edges[edge]['color'] = col
 
 '''Add sizing information into networks'''
-min_node_size = 5
+min_node_size = 10
 node_size_factor = 1e5
 for n in networks:
-	for node in networks[n].nodes:
-		if '>>' in node:
-			networks[n].nodes[node]['size'] = min_node_size
-		elif node + '/ M' in average_data.columns:
-			conc = average_data.loc[n,node +'/ M']
-			amp = amplitude_data.loc[n,node +'/ M']
-			networks[n].nodes[node]['size'] = amp*node_size_factor
-		else:
-			networks[n].nodes[node]['size'] = min_node_size
+    for node in networks[n].nodes:
+        if '>>' in node:
+            networks[n].nodes[node]['size'] = min_node_size
+        elif node + '/ M' in average_data.columns:
+            conc = average_data.loc[n,node +'/ M']
+            amp = amplitude_data.loc[n,node +'/ M']
+            networks[n].nodes[node]['size'] = conc*node_size_factor
+        else:
+            networks[n].nodes[node]['size'] = min_node_size
 
 '''Plotting series in four panels'''
 fig_width = 10/2.54 # cm conversion to inches for plt
@@ -136,78 +135,78 @@ fig_height = 10/2.54 # cm conversion to inches for plt
 base_linew = 0.5
 
 fig,ax = plt.subplots(ncols = 2,
-				figsize = (fig_width, fig_height))
+                figsize = (fig_width, fig_height))
 axes = ax.flatten()
 
 for c,n in enumerate(networks):
-	axes[c].plot(base_net_plot[0],base_net_plot[1],
-				c = '#acb5ad',
-				linewidth = base_linew,
-				zorder = 0)
+    axes[c].plot(base_net_plot[0],base_net_plot[1],
+                c = '#acb5ad',
+                linewidth = base_linew,
+                zorder = 0)
 
-	for e in networks[n].edges:
-	    arrow = FancyArrowPatch(networks[n].nodes[e[0]]['pos'],
-	                            networks[n].nodes[e[1]]['pos'],
-	                            arrowstyle='-|>',
-	                            path = None,
-	                            connectionstyle='Arc',
-	                            facecolor = networks[n].edges[e]['color'],
-	                            edgecolor = networks[n].edges[e]['color'],
-	                            linewidth = 1,
-	                            mutation_scale = 5,
-	                            shrinkA = 2,
-	                            shrinkB = 1,
-	                            alpha = 1,
-								zorder = 1)
+    for e in networks[n].edges:
+        arrow = FancyArrowPatch(networks[n].nodes[e[0]]['pos'],
+                                networks[n].nodes[e[1]]['pos'],
+                                arrowstyle='-|>',
+                                path = None,
+                                connectionstyle='Arc',
+                                facecolor = networks[n].edges[e]['color'],
+                                edgecolor = networks[n].edges[e]['color'],
+                                linewidth = 1,
+                                mutation_scale = 5,
+                                shrinkA = 2,
+                                shrinkB = 1,
+                                alpha = 1,
+                                zorder = 1)
 
-	    axes[c].add_patch(arrow)
+        axes[c].add_patch(arrow)
 
-	# build node scatter
-	compound_nodes_x = []
-	compound_nodes_y = []
-	compound_node_colours = []
-	compound_node_sizes = []
+    # build node scatter
+    compound_nodes_x = []
+    compound_nodes_y = []
+    compound_node_colours = []
+    compound_node_sizes = []
 
-	reaction_nodes_x = []
-	reaction_nodes_y = []
+    reaction_nodes_x = []
+    reaction_nodes_y = []
 
-	for node in networks[n].nodes:
-		if '>>' in node:
-			reaction_nodes_x.append(networks[n].nodes[node]['pos'][0])
-			reaction_nodes_y.append(networks[n].nodes[node]['pos'][1])
-		else:
-			compound_nodes_x.append(networks[n].nodes[node]['pos'][0])
-			compound_nodes_y.append(networks[n].nodes[node]['pos'][1])
-			compound_node_colours.append(networks[n].nodes[node]['color'])
-			compound_node_sizes.append(networks[n].nodes[node]['size'])
+    for node in networks[n].nodes:
+        if '>>' in node:
+            reaction_nodes_x.append(networks[n].nodes[node]['pos'][0])
+            reaction_nodes_y.append(networks[n].nodes[node]['pos'][1])
+        else:
+            compound_nodes_x.append(networks[n].nodes[node]['pos'][0])
+            compound_nodes_y.append(networks[n].nodes[node]['pos'][1])
+            compound_node_colours.append(networks[n].nodes[node]['color'])
+            compound_node_sizes.append(networks[n].nodes[node]['size'])
 
-	# plot solid scatter for compound concentrations
-	axes[c].scatter(compound_nodes_x, compound_nodes_y,
-				facecolors = compound_node_colours,
-				s = compound_node_sizes,
-				zorder = 2,
-				edgecolors = 'None',
-				alpha = 1)
+    # plot solid scatter for compound concentrations
+    axes[c].scatter(compound_nodes_x, compound_nodes_y,
+                facecolors = compound_node_colours,
+                s = compound_node_sizes,
+                zorder = 2,
+                edgecolors = 'None',
+                alpha = 1)
 
-	axes[c].scatter(reaction_nodes_x, reaction_nodes_y,
-					c = '#000000',
-					s = min_node_size,
-					marker = 'D',
-					edgecolors = 'None',
-					zorder = 2,
-					alpha = 1)
+    axes[c].scatter(reaction_nodes_x, reaction_nodes_y,
+                    c = '#000000',
+                    s = min_node_size,
+                    marker = 'D',
+                    edgecolors = 'None',
+                    zorder = 2,
+                    alpha = 1)
 
-	axes[c].set_axis_off()
+    axes[c].set_axis_off()
 
-	# optional annotations
-	# for node in networks[n].nodes:
-	# 	if node in info_params.compound_numbering:
-	# 		number = info_params.compound_numbering[node]
-	# 		axes[c].annotate(number, xy = networks[n].nodes[node]['pos'],
-	# 						ha = 'center', va = 'center',
-	# 						fontsize = 6)
+    # optional annotations
+    # for node in networks[n].nodes:
+    #   if node in info_params.compound_numbering:
+    #       number = info_params.compound_numbering[node]
+    #       axes[c].annotate(number, xy = networks[n].nodes[node]['pos'],
+    #                       ha = 'center', va = 'center',
+    #                       fontsize = 6)
 
 fig.tight_layout()
 plt.savefig(plot_folder/'{}.png'.format(file_name), dpi = 600)
 plt.savefig(plot_folder/'{}.svg'.format(file_name))
-plt.show()
+plt.close()
